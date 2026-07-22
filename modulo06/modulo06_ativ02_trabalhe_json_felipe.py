6ba493d9a5d4fd9b072641e26e73597d4ed3cd35
andrade011 <felipemendesdopradosantos@gmail.com>
2026-07-22 11:59:14 -0300

    updade
    Aprendemos oTXT

---
+++ modulo06/modulo06_ativ02_trabalhe_json_felipe.py
@@ -0,0 +1,32 @@
+import json
+
+nome_arquivo = "clientes_nomes.json"
+
+# Dicionário/Lista com os dados dos clientes
+clientes = [
+    {
+        "Nome completo": "Ivan Silva",
+        "idade": "40 anos",
+        "CEP": "02899-000",
+        "ResgMatr": "947541",
+        "E-Mail": "ivanpaulino@mail.com"
+    },
+    {
+        "Nome completo": "Beatriz Vitoria",
+        "idade": "30 anos",
+        "CEP": "057193-000",
+        "ResgMatr": "978786",
+        "E-Mail": "beavitoria@mail.com"
+    },
+    {
+        "Nome completo": "Eric Renan",
+        "idade": "17 anos",
+        "CEP": "089880-100",
+        "ResgMatr": "98799",
+        "E-Mail": "ericrenan@gmail.com"
+    }
+]
+
+# --- ESCRITA (Salvar) ---
+with open(nome_arquivo, "w", encoding="utf-8") as arquivo:
+    json.dump(clientes, arquiv…
